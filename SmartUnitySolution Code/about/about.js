516f3a5709378755511433ac282e4643618c860e
JinMinLeong <jinminleong@gmail.com>
2025-12-10 18:08:20 +0800

    Add files via upload

---
+++ SmartUnitySolution Code/about/about.js
@@ -0,0 +1,6 @@
+// About Page Script
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any about page specific functionality here
+    console.log('About page loaded');
+});
+
---
+++ SmartUnitySolution Code/home/home.js
@@ -0,0 +1,6 @@
+// Home Page Script
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any home page specific functionality here
+    console.log('Home page loaded');
+});
+
