4d2f508b24c69b2d7e5b00fab3ee261017c31039
JinMinLeong <jinminleong@gmail.com>
2025-12-12 12:48:12 +0800

    Delete SmartUnitySolution Code directory

--- SmartUnitySolution Code/about/about.js
+++
@@ -1,6 +0,0 @@
-// About Page Script
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any about page specific functionality here
-    console.log('About page loaded');
-});
-
--- SmartUnitySolution Code/home/home.js
+++
@@ -1,6 +0,0 @@
-// Home Page Script
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any home page specific functionality here
-    console.log('Home page loaded');
-});
-
